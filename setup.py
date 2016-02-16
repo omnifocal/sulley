8241bac3f4d3f592a32773f5f87f33eda334f862
Jack Turner <jack.turner@ecu.edu.au>
2016-02-16 16:43:07 +0800

    Quick and dirty setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='sulley',
+      version='master-3bce87a',
+      description='Python fuzzing framework',
+      author='Pedram Amini',
+      author_email='pedram.amini@gmail.com',
+      url='https://github.com/OpenRCE/sulley.git',
+      packages=['sulley','sulley.legos','sulley.utils','sulley.pgraph']
+      )
